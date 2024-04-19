cde3438f8dd2f03311db0aa0b06e150916670968
yuhwanwoo <6514687@naver.com>
2024-04-19 19:20:22 +0900

    클래스 로더 확인

--- src/main/java/com/thejava/App.java
+++ src/main/java/com/thejava/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader); // App Class Loader
+        System.out.println(classLoader.getParent()); // Platform Class Loader
+        System.out.println(classLoader.getParent().getParent()); // 있지만 네이티브 코드라 구현되어 있고 VM 마다 달라서 볼 수 없다. (부트스트랩 클래스 로더)
     }
 }
