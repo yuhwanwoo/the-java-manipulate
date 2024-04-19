ab5221fb44afb9225525f51bb10a5c83403798c8
yuhwanwoo <6514687@naver.com>
2024-04-19 19:51:34 +0900

    jacoco 설정

---
+++ src/main/java/com/thejava/Book.java
@@ -0,0 +1,4 @@
+package com.thejava;
+
+public class Book {
+}
---
+++ src/test/java/com/thejava/MoimTest.java
@@ -0,0 +1,16 @@
+package com.thejava;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoimTest {
+
+    @Test
+    public void isFull() {
+        Moim moim = new Moim();
+        moim.maxNumberOfAttendees = 100;
+        moim.numberOfEnrollment = 10;
+        Assert.assertFalse(moim.isEnrollmentFull());
+    }
+}
